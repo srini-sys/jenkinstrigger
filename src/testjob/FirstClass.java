73a3ad84e8a0760d6a61a60a48e1f662b556d08d
srini <prabu.srinivas1@gmail.com>
2021-03-19 16:22:35 +0530

    Hello Jenkins

--- src/testjob/FirstClass.java
+++ src/testjob/FirstClass.java
@@ -1,9 +1,14 @@
 package testjob;
 
 public class FirstClass {
+	
+	public static void hellojenkins(){
+		System.out.println("hello jenkins");
+	}
 
 	public static void main(String args[]) {
 		System.out.println("Hello Java");
+		hellojenkins();
 	}
 
 }
